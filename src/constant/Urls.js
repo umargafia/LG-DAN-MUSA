c30ac7d17deb625c996aaf02cd6a61cf3783b246
umar musa <umargafia@gmail.com>
2023-12-30 17:49:22 +0100

    initial commit

---
+++ src/constant/Urls.js
@@ -0,0 +1,11 @@
+const baseUrl = 'https://lgdanmusa.com/';
+
+export default URLS = () => {
+  return {
+    home: `${baseUrl}mobile/home/`,
+    login: `${baseUrl}mobile/login/`,
+    forgetPassword: `${baseUrl}mobile/recovery/`,
+    signup: `${baseUrl}mobile/register/`,
+    api: `${baseUrl}api/account/login/`,
+  };
+};
